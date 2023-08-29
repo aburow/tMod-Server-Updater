77c3739ab67af46d702ac04ff46a9b33922d20e0
aburow <anthony@gosman-rowe.com>
2023-08-30 00:32:19 +1000

    Update boot_start.sh
    comments

--- boot_start.sh
+++ boot_start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Anthony Burow
+# 
+# This file sets up tmux for tmod to run in the background on a virtual terminal
+# The file is meant to be called from crontab after every restart of an LXC
+#
+# called by: crontab
+# calls: start.sh
+
 tmux new -d -s tmod
 tmux send-keys -t tmod:. "cd /root/tModLoader" C-m
 tmux send-keys -t tmod:. "./start.sh boot" C-m
